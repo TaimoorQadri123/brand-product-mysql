9635f9c93d3877ff7b598c832a5a3ed120ff8a24
TaimoorQadri123 <dchota374@gmail.com>
2024-12-06 23:17:23 +0500

    final

--- New folder/2bdsm.sql
+++ New folder/2bdsm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2024 at 07:16 PM
+-- Generation Time: Dec 06, 2024 at 06:54 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.28
 
